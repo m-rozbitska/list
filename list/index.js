1c2dc2d64776e2b1b7c1c49a2ff3b41d57dcf94b
Mariia Rozbitska <melissa180lily@gmail.com>
2023-04-08 23:34:38 +0300

    feat: add changes

--- list/index.js
+++ list/index.js
@@ -1,10 +1,8 @@
 function ready() {
 
-	//призначаємо подію обєкту
 tree.addEventListener('click', function(event){
 	let childrenShelter = event.target.parentNode.querySelector('ul');
 	let target = event.target;
-	//let CloseEverything = target.closest('.list').querySelector('ul');
 
 	if (target.tagName == 'SPAN') {
 		childrenShelter.hidden = !childrenShelter.hidden;
@@ -23,34 +21,3 @@ tree.addEventListener('click', function(event){
 };
 document.addEventListener('DOMContentLoaded', ready);
 
-//tree.onclick = function(event) {
-		//let childrenContainer = event.target.parentNode.querySelector('ul');
-		//let target = event.target;
-	
-		//if (target.tagName == 'SPAN' && childrenContainer) {
-			//console.log(childrenContainer);
-			//childrenContainer.hidden = !childrenContainer.hidden;
-		//}
-	
-		//if (target.tagName == 'SPAN' && !childrenContainer) {
-			//let btnTagretAllHidden = target.closest('.list');
-			//btnTagretAllHidden.hidden = !btnTagretAllHidden.hidden;
-		//}
-	
-		//if (target.tagName == 'BUTTON' && childrenContainer) {
-			//childrenContainer.hidden = !childrenContainer.hidden;
-		//}
-	//};
-		//if (target.tagName == 'BUTTON' && !childrenShelter) {
-		// let btnTagretColorChangeRed = target.closest('.list');
-		//target.style.color = "red";
-	//};
-
-	//if(target.tagName == 'BUTTON' && childrenShelter) {
-		//target.style.color = "green";
-	//};
-
-	// if (target.tagName == 'SPAN' && !childrenShelter) {
-	// 	let btnTagretAllHidden = target.closest('.list');
-	//    btnTagretAllHidden.hidden = !btnTagretAllHidden.hidden;
-	// };
